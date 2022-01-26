fd06e378124adc9dbf20ce5ddca11e0b69e2b8c7
deletel <ars1234@mail.ru>
2022-01-26 14:43:02 +0300

    Update Message.java

--- sweater/src/main/java/com/example/sweater/domain/Message.java
+++ sweater/src/main/java/com/example/sweater/domain/Message.java
@@ -1,27 +1,39 @@
 package com.example.sweater.domain;
 
+
+import com.example.sweater.domain.util.MessageHelper;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    public Long id;
+    @GeneratedValue(strategy=GenerationType.AUTO) //AUTO
+    private Long id;
 
     @NotBlank(message = "Please fill the message")
     @Length(max = 2048, message = "Message too long")
-    public String text;
+    private String text;
     @Length(max = 255, message = "Message too long")
-    public String tag;
+    private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
-    public User author;
+    private User author;
 
     private String filename;
+
+    @ManyToMany
+    @JoinTable(
+            name = "message_likes",
+            joinColumns = { @JoinColumn(name = "message_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id")}
+    )
+    private Set<User> likes = new HashSet<>();
 
     public User getAuthor() {return author;}
 
@@ -55,7 +67,7 @@ public class Message {
         this.tag = tag;
     }
     public String getAuthorName() {
-        return author !=null ? author.getUsername() : "<none>";
+        return MessageHelper.getAuthorname(author);
     }
 
 
@@ -67,5 +79,13 @@ public class Message {
 
     public void setFilename(String filename) {
         this.filename = filename;
+    }
+
+    public Set<User> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<User> likes) {
+        this.likes = likes;
     }
 }
